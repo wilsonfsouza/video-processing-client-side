3484f612e53a5dc76c9be87ace1bb69a5b80e31e
Wilson Franca <wilson.franca.92@gmail.com>
2023-10-27 07:44:20 -0700

    feat(app): added encoder config to worker and created video processor class to handle video processing

---
+++ initial-template/app/pages/file-upload/src/worker/videoProcessor.js
@@ -0,0 +1,5 @@
+export default class VideoProcessor {
+  async start({file, encoderConfig}) {
+    debugger
+  }
+}
--- initial-template/app/pages/file-upload/src/worker/worker.js
+++ initial-template/app/pages/file-upload/src/worker/worker.js
@@ -1,8 +1,42 @@
-onmessage = ({data}) => {
-  setTimeout(() => {
-    self.postMessage({
-      status: 'done'
-    })
-  }, 2000)
-  
+import VideoProcessor from "./videoProcessor.js"
+
+// qvga = 144p on youtube
+const qvgaConstraints = {
+  width: 320,
+  height: 240,
+}
+const vgaConstraints = {
+  width: 640,
+  height: 480,
+}
+const hdConstraints = {
+  width: 1280,
+  height: 720,
+}
+
+const encoderConfig = {
+  ...qvgaConstraints,
+  bitrate: 10e6,
+  // WebM
+  codec: 'vp09.00.10.08',
+  pt: 4,
+  hardwareAccelaration: 'prefer-software',
+
+  // MP4
+  // codec: 'avc1.42002A',
+  // pt: 1,
+  // hardwareAccelaration: 'prefer-hardware',
+  // avc: { format: 'annexb' }
+}
+
+const videoProcessor = new VideoProcessor()
+
+onmessage = async ({data}) => {
+  await videoProcessor.start({
+    file: data.file,
+    encoderConfig
+  })
+  self.postMessage({
+    status: 'done'
+  })
 }
