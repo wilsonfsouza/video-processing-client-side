70b0445c01bf95a7fc615fa440fe300507034812
Wilson Franca <wilson.franca.92@gmail.com>
2023-10-29 16:29:33 -0700

    feat(videoProcessor): created a class to read the file on demand using the mp4Decoder

--- initial-template/app/pages/file-upload/src/worker/videoProcessor.js
+++ initial-template/app/pages/file-upload/src/worker/videoProcessor.js
@@ -9,22 +9,54 @@ export default class VideoProcessor {
     this.#mp4Demuxer = mp4Demuxer
   }
 
-  async mp4Decoder(encoderConfig, stream) {
-    this.#mp4Demuxer.run(stream, {
-      onConfig(config) {
-        debugger
-      },
-      onChunk(config) {
-        debugger
+  /**
+   * @returns {ReadableStream}
+   */
+  mp4Decoder(encoderConfig, stream) {
+    return new ReadableStream({
+      start: async (controller) => {
+        const decoder = new VideoDecoder({
+          /**
+           * @param {VideoFrame} frame 
+           */
+          output(frame) {
+            controller.enqueue(frame)
+          },
+          error(e) {
+            console.error('Error at MP4Decoder', e)
+            controller.error(e)
+          }
+        })
+    
+        return this.#mp4Demuxer.run(stream, {
+          onConfig(config) {
+            decoder.configure(config)
+          },
+          /**
+           * 
+           * @param {EncodedVideoChunk} chunk 
+           */
+          onChunk(chunk) {
+            decoder.decode(chunk)
+          }
+        }).then(() => {
+          // Temporary workaround to stop countdown
+          setTimeout(() => {
+            controller.close()
+          }, 1000)
+        })
       }
     })
   }
 
-  async start({ file, encoderConfig, sendMessage }) {
+  async start({ file, encoderConfig, renderFrame }) {
     const stream = file.stream()
     const fileName = file.name.split('/').pop().replace('.mp4', '')
 
-    await this.mp4Decoder(encoderConfig, stream)
-    debugger
+    await this.mp4Decoder(encoderConfig, stream).pipeTo(new WritableStream({
+      write(frame) {
+        renderFrame(frame)
+      }
+    }))
   }
 }
