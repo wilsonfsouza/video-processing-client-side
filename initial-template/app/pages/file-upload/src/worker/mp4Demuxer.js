ff2e95e5b82246b68c03e312695329faad919e0e
Wilson Franca <wilson.franca.92@gmail.com>
2023-10-29 16:27:11 -0700

    feat(mp4Demuxer): created demuxer to read file on demand, getting its original codification informationto encode/transform it into the desirable format and rendering the processed frame.

--- initial-template/app/pages/file-upload/src/worker/mp4Demuxer.js
+++ initial-template/app/pages/file-upload/src/worker/mp4Demuxer.js
@@ -1,4 +1,4 @@
-import { createFile } from '../deps/mp4box.0.5.2.js'
+import { createFile, DataStream } from '../deps/mp4box.0.5.2.js'
 
 export default class MP4Demuxer {
   #onConfig 
@@ -18,17 +18,85 @@ export default class MP4Demuxer {
     this.#onChunk = onChunk
 
     this.#file = createFile()
-    this.#file.onReady = (arguments) => {
-      debugger
-    }
+    this.#file.onReady = this.#onReady.bind(this)
+
+    this.#file.onSamples = this.#onSamples.bind(this)
 
     this.#file.onError = (error) => 
       console.error('Bad request mp4Demuxer', error)
     
-    this.#init(stream)
+    return this.#init(stream)
   }
 
-  #init(stream) {
+  /**
+   * 
+   * @param {JSON} info 
+   * @param {object[]} info.videoTracks
+   */
+  #onReady(info) {
+    const [track] = info.videoTracks
+    
+    this.#onConfig({
+      codec: track.codec,
+      codedHeight: track.video.height,
+      codedWidth: track.video.width,
+      description: this.#description(track),
+      durationSecs: info.duration / info.timescale
+    })
+    this.#file.setExtractionOptions(track.id)
+    this.#file.start()
+  }
 
+  // Get the appropriate `description` for a specific track. Assumes that the
+  // track is H.264, H.265, VP8, VP9, or AV1.
+  #description({ id }) {
+    const track = this.#file.getTrackById(id);
+    for (const entry of track.mdia.minf.stbl.stsd.entries) {
+      const box = entry.avcC || entry.hvcC || entry.vpcC || entry.av1C;
+      if (box) {
+        const stream = new DataStream(undefined, 0, DataStream.BIG_ENDIAN);
+        box.write(stream);
+        return new Uint8Array(stream.buffer, 8);  // Remove the box header.
+      }
+    }
+    throw new Error("avcC, hvcC, vpcC, or av1C box not found");
+  }
+
+  #onSamples(trackId, ref, samples) {
+    // Generate and emit an EncodedVideoChunk for each demuxed sample.
+    for (const sample of samples) {
+      this.#onChunk(new EncodedVideoChunk({
+        type: sample.is_sync ? "key" : "delta",
+        timestamp: 1e6 * sample.cts / sample.timescale,
+        duration: 1e6 * sample.duration / sample.timescale,
+        data: sample.data
+      }));
+    }
+  }
+  /**
+   * @param {ReadableStream} stream
+   * @returns {Promise<void>}
+   */
+  #init(stream) {
+    let _offeset = 0
+
+    const consumeFile = new WritableStream({
+      /**
+       * 
+       * @param {Uint8Array} chunk 
+       */
+      write: (chunk) => {
+        const copy = chunk.buffer
+        copy.fileStart = _offeset
+        this.#file.appendBuffer(copy)
+
+        _offeset += chunk.length
+      },
+      close: () => {
+        this.#file.flush()
+      }
+    })
+
+    return stream.pipeTo(consumeFile)
   }
 }
