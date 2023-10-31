dd6b4767ec1285c03ef91816490d5d98432aeccd
Wilson Franca <wilson.franca.92@gmail.com>
2023-10-31 07:08:48 -0700

    feat(app/videoProcessor): created method to encode video to 144p

--- initial-template/app/pages/file-upload/src/worker/videoProcessor.js
+++ initial-template/app/pages/file-upload/src/worker/videoProcessor.js
@@ -12,7 +12,7 @@ export default class VideoProcessor {
   /**
    * @returns {ReadableStream}
    */
-  mp4Decoder(encoderConfig, stream) {
+  mp4Decoder(stream) {
     return new ReadableStream({
       start: async (controller) => {
         const decoder = new VideoDecoder({
@@ -49,15 +49,73 @@ export default class VideoProcessor {
     })
   }
 
+  encode144p(encodeConfig) {
+    let _encoder;
+
+    const readable = new ReadableStream({
+      start: async (controller) => {
+        const { supported } = await VideoEncoder.isConfigSupported(encodeConfig)
+
+        if (!supported) {
+          const message = 'encode144p VideoEncoder config is not supported'
+          console.error(message, encodeConfig)
+          controller.error(message)
+          return
+        }
+
+        _encoder = new VideoEncoder({
+          /**
+           * 
+           * @param {EncodedVideoChunk} frame 
+           * @param {EncodedVideoChunkMetadata} config 
+           */
+          output: (frame, config) => {
+            if (config.decoderConfig) {
+              const decoderConfig = {
+                type: 'config',
+                config: config.decoderConfig
+              }
+
+              controller.enqueue(decoderConfig)
+            }
+
+            controller.enqueue(frame)
+          },
+          error: (err) => {
+            console.error('VideoEncoder 144p', err)
+            controller.error(err)
+          }
+        })
+
+        await _encoder.configure(encodeConfig)
+      }
+
+    })
+
+    const writable = new WritableStream({
+      async write(frame) {
+        _encoder.encode(frame)
+        frame.close()
+      }
+    })
+
+    return {
+      readable,
+      writable
+    }
+  }
+
   async start({ file, encoderConfig, renderFrame, sendMessage }) {
     const stream = file.stream()
     const fileName = file.name.split('/').pop().replace('.mp4', '')
 
-    await this.mp4Decoder(encoderConfig, stream).pipeTo(new WritableStream({
-      write(frame) {
-        renderFrame(frame)
-      }
-    }))
+    await this.mp4Decoder(stream)
+      .pipeThrough(this.encode144p(encoderConfig))
+      .pipeTo(new WritableStream({
+        write(frame) {
+          renderFrame(frame)
+        }
+      }))
 
     sendMessage({
       status: 'done'
